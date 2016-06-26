d55d9a3556625c083514aaa8d836307142fa7563
Sophiya Maharjan <deltasophiya@gmail.com>
2016-06-26 14:20:43 +0545

    models for discussion forum

---
+++ OnlineCourse/OnlineCourse/Models/DiscussionForum.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace OnlineCourse.Models
+{
+    public class DiscussionForum
+    {
+        public int DiscussionForumID { get; set; }
+
+        public string DiscussionTitle { get; set; }
+
+        public string DiscussionDiscription { get; set; }
+
+        public int UserID { get; set; }
+
+        public string PostDate { get; set; }
+    }
+}
