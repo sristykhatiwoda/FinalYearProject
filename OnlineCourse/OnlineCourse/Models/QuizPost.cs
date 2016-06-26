481f15db4cece955bf284794b6d22574221db5de
Sophiya Maharjan <deltasophiya@gmail.com>
2016-06-26 14:22:42 +0545

    QuizPost has been added

---
+++ OnlineCourse/OnlineCourse/Models/QuizPost.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace OnlineCourse.Models
+{
+    public class QuizPost
+    {
+        public int QuizPostID { get; set; }
+
+        public int QuizID { get; set; }
+
+        public int StudentID { get; set; }
+
+        public int QuizPoint { get; set; }
+    }
+}
